19cf44ab610a23f4baa14e6004ae2c76395b1608
Juho Kalberg <juss@vilenkell.eu>
2023-03-21 08:15:18 +0200

    reset typo styles

--- script copy.js
+++ script copy.js
@@ -1,21 +1,16 @@
-const header = document.querySelector('header');
+const hero = document.querySelector('.hero');
 const fadeStart = 0;
-const fadeEnd = window.innerHeight * 0.7; // 70% of the window height
+const fadeEnd = window.innerHeight * 0.7; // 50% of the window height, it can be also value in px
 const fadeRange = fadeEnd - fadeStart;
 
 window.addEventListener('scroll', () => {
   const currentScroll = window.pageYOffset;
   if (currentScroll <= fadeStart) {
-    header.classList.remove('fade');
-    header.style.opacity = 1;
-    header.style.filter = 'blur(0)';
+    hero.style.opacity = 1;
   } else if (currentScroll >= fadeEnd) {
-    header.classList.add('fade');
+    hero.style.opacity = 0;
   } else {
     const opacity = 1 - (currentScroll - fadeStart) / fadeRange;
-    header.style.opacity = opacity.toFixed(2);
-    const blur = 20 * (1 - opacity.toFixed(2));
-    header.style.filter = `blur(${blur}px)`;
-    header.classList.remove('fade');
+    hero.style.opacity = opacity.toFixed(2);
   }
 });
